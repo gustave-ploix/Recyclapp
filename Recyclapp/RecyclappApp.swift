fa31c04c4680e9104ef3f32d8d84fb559bfca139
gustave ploix <gustaveploix.pro@gmail.com>
2022-03-23 11:56:01 +0100

    Initial Commit

---
+++ Recyclapp/RecyclappApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecyclappApp.swift
+//  Recyclapp
+//
+//  Created by Apprenant 61 on 23/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct RecyclappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
