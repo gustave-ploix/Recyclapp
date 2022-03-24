562bfaf16dde10ce2a48441cc40fa1e5693e183b
gustave ploix <gustaveploix.pro@gmail.com>
2022-03-24 14:12:15 +0100

    added the camera display directly to the root page and a button to cancel the current photo

--- Recyclapp/Pages/CameraView.swift
+++ Recyclapp/Pages/CameraView.swift
@@ -20,27 +20,31 @@ struct CameraView: View {
                     .resizable()
                     .scaledToFill ()
                     .ignoresSafeArea()
-            } else {
-                Color(UIColor.systemBackground)
-            }
-            
-            VStack {
-                Spacer()
-                Button {
-                    isCustomCameraViewPresented.toggle()
-                } label: {
-                    Image(systemName: "camera.fill")
-                        .font(.largeTitle)
-                        .padding()
-                        .background(Color.black)
-                        .foregroundColor(.white)
-                        .clipShape(Circle())
-                }
-                .padding(.bottom)
-                .sheet(isPresented: $isCustomCameraViewPresented, content: {
-                    CustomCameraView(capturedImage: $capturedImage)
-                })
                 
+                /*display a button at the top left
+                 that delete the current image
+                 */
+                HStack {
+                    VStack {
+                        Button {
+                            capturedImage = nil
+                        } label: {
+                            Image(systemName: "chevron.left.circle.fill")
+                                .foregroundColor(.white)
+                                .font(.system(size: 44))
+                                .padding()
+                        } // Label
+
+                        
+                        
+                        Spacer() // Push to the top
+                    } // hstack
+                    Spacer() // push to the left
+                } // vstack
+                
+            } else {
+                // display the camera component
+                CustomCameraView(capturedImage: $capturedImage)
             }
             
         } // ZStack
