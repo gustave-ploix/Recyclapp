1bd011f17f23b097c71ab100dea5992dc8520e7e
gustave ploix <gustaveploix.pro@gmail.com>
2022-03-25 09:59:36 +0100

    added a classifier structure, need to implement in the view

---
+++ Recyclapp/Services/ClassifierCoreML.swift
@@ -0,0 +1,39 @@
+//
+//  ClassifierCoreML.swift
+//  Recyclapp
+//
+//  Created by Apprenant 61 on 25/03/2022.
+//
+
+import CoreML
+import Vision
+import CoreImage
+
+struct Classifier {
+    
+    private(set) var results: String
+    
+        
+    mutating func detectImage(ciImage: CIImage) {
+        guard let model = try? VNCoreMLModel(for: Resnet50(configuration: MLModelConfiguration()).model)
+        else {
+            return
+        }
+        
+        let request = VNCoreMLRequest(model: model)
+        
+        let handler = VNImageRequestHandler(ciImage: ciImage, options: [:])
+        
+        try? handler.perform([request])
+        
+        guard let result = request.results as? [VNClassificationObservation] else {
+            return
+        }
+        
+        if let firstResult = result.first {
+            self.results = firstResult.identifier
+        }
+    }
+    
+}
+
